6be1f3d135adf76b5cf605536beed8dbda08e38c
Casey The Great <casey@caseypitman.com>
2020-12-13 10:03:19 -0600

    Header: dynamically render x-logo (light or dark) based on current theme.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,31 @@
 //Header component
 
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 //Components
 import Toggle from "./Toggle";
 
 //Assets
 
-import darkLogo from '../images/x_logo_dark.jpg'
-import lightLogo from '../images/x_logo_light.jpg'
+import darkLogo from "../images/x_logo_dark.jpg";
+import lightLogo from "../images/x_logo_light.jpg";
 
 //Styles
-import '../css/header.css'
+import "../css/header.css";
 
-const Header = ({theme, toggleTheme}) => {
+const Header = ({ theme, toggleTheme }) => {
+  const [logo, setLogo] = useState(lightLogo);
 
-  const [logo, setLogo] = useState(lightLogo)
-
-  
+  useEffect(() => {
+    const curLogo = theme === "light" ? lightLogo : darkLogo;
+    setLogo(curLogo);
+  }, [theme]);
 
   return (
     <div className='header'>
       <div className='header-top'></div>
       <div className='header-content'>
-        <img className = 'x-logo' src = {logo} alt = 'X logo.'/>
+        <img className='x-logo' src={logo} alt='X logo.' />
         <p className='info-tag intro-tag'>[krakoan_native]..[000]</p>
         <h1 className='headline'>Krakoan Translator</h1>
         <div className='header-info'>
