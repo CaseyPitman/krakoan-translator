950f1dace915299bc5fc6f0941105f809afa840e
Casey The Great <casey@caseypitman.com>
2020-12-13 09:57:00 -0600

    Header: added x-logo to Header. Sized and placed.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,37 @@
 //Header component
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //Components
 import Toggle from "./Toggle";
 
+//Assets
+
+import darkLogo from '../images/x_logo_dark.jpg'
+import lightLogo from '../images/x_logo_light.jpg'
+
 //Styles
 import '../css/header.css'
 
-const Header = props => {
+const Header = ({theme, toggleTheme}) => {
+
+  const [logo, setLogo] = useState(lightLogo)
+
+  
+
   return (
     <div className='header'>
       <div className='header-top'></div>
       <div className='header-content'>
+        <img className = 'x-logo' src = {logo} alt = 'X logo.'/>
+        <p className='info-tag intro-tag'>[krakoan_native]..[000]</p>
         <h1 className='headline'>Krakoan Translator</h1>
         <div className='header-info'>
           <p className='info-tag'>[krakoan_init]....[exe]</p>
           <div className='display-mode-switch-container'>
             <Toggle
-              toggleTheme={props.toggleTheme}
-              theme={props.theme}
+              toggleTheme={toggleTheme}
+              theme={theme}
               className='toggle-button'
             />
           </div>
