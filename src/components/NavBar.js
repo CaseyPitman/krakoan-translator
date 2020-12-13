ccf676b4de8727a76799ff72ce89e8b9018ac940
Casey The Great <casey@caseypitman.com>
2020-12-13 11:32:36 -0600

    NavBar: basic setup, no links or dyanmic styling yet.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,15 +3,18 @@
 import React, { useState, useEffect } from "react";
 
 //Styles
-import '../css/navBar.css'
+import "../css/navBar.css";
 
 const NavBar = props => {
   return (
     <div class='nav-bar'>
-      <h1>Navbar</h1>
+      <div class='nav-link-container'>
+        <div className='nav-bar-home nav-link active'>Home</div>
+        <div className='nav-bar-e-to-k nav-link'>English to Krakoan</div>
+        <div className='nav-bar-k-to-e nav-link'>Krakoan to English</div>
+      </div>
     </div>
   );
 };
-
 
 export default NavBar;
