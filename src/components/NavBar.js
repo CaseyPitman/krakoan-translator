5c83c321ecd8544acf5a7167e27d978c5cb8142e
Casey The Great <casey@caseypitman.com>
2020-12-14 15:55:34 -0600

    NavBar - switched Link to NavLink and added activeClassName styling.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,34 @@
 //NavBar component
 
 import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 //Styles
 import "../css/navBar.css";
 
 const NavBar = props => {
-
   const location = useLocation();
-  console.log(location.pathname)
-  const clickLink = () => {
-
-  }
+  console.log(location.pathname);
+  const clickLink = () => {};
 
   return (
     <div className='nav-bar'>
       <div className='nav-link-container'>
-        <Link to='/'>
-          <div className='nav-link' onClick = {clickLink}>
-            Home
-          </div>
-        </Link>
-        <Link to='/english-to-krakoan'>
-          <div className='nav-link' onClick = {clickLink}>
-            English to Krakoan
-          </div>
-        </Link>
-        <Link to='/krakoan-to-english'>
-          <div className='nav-link' onClick = {clickLink}>
-            Krakoan to English
-          </div>
-        </Link>
+        <NavLink exact to='/' className='nav-link' activeClassName='active'>
+          Home
+        </NavLink>
+        <NavLink
+          to='/english-to-krakoan'
+          className='nav-link'
+          activeClassName='active'>
+          English to Krakoan
+        </NavLink>
+        <NavLink
+          to='/krakoan-to-english'
+          className='nav-link'
+          activeClassName='active'>
+          Krakoan to English
+        </NavLink>
       </div>
     </div>
   );
