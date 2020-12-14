89a45c13769b274386ee6202cff4cbb3caf2747e
Casey The Great <casey@caseypitman.com>
2020-12-14 15:36:07 -0600

    Used useLocation to determine current pathname.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,36 @@
 //NavBar component
 
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 //Styles
 import "../css/navBar.css";
 
 const NavBar = props => {
+
+  const location = useLocation();
+  console.log(location.pathname)
+  const clickLink = () => {
+
+  }
+
   return (
     <div className='nav-bar'>
       <div className='nav-link-container'>
         <Link to='/'>
-          <div className='nav-link'>Home</div>
+          <div className='nav-link' onClick = {clickLink}>
+            Home
+          </div>
         </Link>
         <Link to='/english-to-krakoan'>
-          <div className='nav-link'>English to Krakoan</div>
+          <div className='nav-link' onClick = {clickLink}>
+            English to Krakoan
+          </div>
         </Link>
         <Link to='/krakoan-to-english'>
-          <div className='nav-link'>Krakoan to English</div>
+          <div className='nav-link' onClick = {clickLink}>
+            Krakoan to English
+          </div>
         </Link>
       </div>
     </div>
