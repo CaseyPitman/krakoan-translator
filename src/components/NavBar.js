88627a3feb8ca5c0826d2f121959cdb65bdd9b14
Casey The Great <casey@caseypitman.com>
2020-12-14 15:18:16 -0600

    Wired up navigation via react-router links.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 //NavBar component
 
 import React, { useState, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 //Styles
 import "../css/navBar.css";
 
@@ -9,9 +9,15 @@ const NavBar = props => {
   return (
     <div className='nav-bar'>
       <div className='nav-link-container'>
-        <div className='nav-link active'>Home</div>
-        <div className='nav-link inactive'>English to Krakoan</div>
-        <div className='nav-link inactive'>Krakoan to English</div>
+        <Link to='/'>
+          <div className='nav-link'>Home</div>
+        </Link>
+        <Link to='/english-to-krakoan'>
+          <div className='nav-link'>English to Krakoan</div>
+        </Link>
+        <Link to='/krakoan-to-english'>
+          <div className='nav-link'>Krakoan to English</div>
+        </Link>
       </div>
     </div>
   );
