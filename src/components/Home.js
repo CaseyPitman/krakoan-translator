3794c3ee007c12ad1c70638601307e3d47efb7cc
Casey The Great <casey@caseypitman.com>
2020-12-15 11:35:22 -0600

    Added partial borders and spaced out dummy text in Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,22 +10,27 @@ const Home = () => {
     <div className='home'>
       <div className='home-data-container'>
         <div className='home-data-top'></div>
-        <p className='data-text'>
-          An intro to krakoan language and explanation. there will be menition
-          of hickman and ramsey as well as explantions on extra characters, etc.
-        </p>
-        <p className='data-text'>
-          Egestas erat imperdiet sed euismod nisi porta lorem mollis. Felis
-          donec et odio pellentesque diam volutpat commodo sed. Ante in nibh
-          mauris cursus mattis molestie a. Risus ultricies tristique nulla
-          aliquet enim tortor at auctor urna. Sed lectus vestibulum mattis.
-        </p>
-        <p className='data-text'>
-          Egestas erat imperdiet sed euismod nisi porta lorem mollis. Felis
-          donec et odio pellentesque diam volutpat commodo sed. Ante in nibh
-          mauris cursus mattis molestie a. Risus ultricies tristique nulla
-          aliquet enim tortor at auctor urna. Sed lectus vestibulum mattis.
-        </p>
+        <div className='data-text-wrapper'>
+          <p className='data-text'>
+            An intro to krakoan language and explanation. there will be menition
+            of hickman and ramsey as well as explantions on extra characters,
+            etc.
+          </p>
+          <br></br>
+          <p className='data-text'>
+            Egestas erat imperdiet sed euismod nisi porta lorem mollis. Felis
+            donec et odio pellentesque diam volutpat commodo sed. Ante in nibh
+            mauris cursus mattis molestie a. Risus ultricies tristique nulla
+            aliquet enim tortor at auctor urna. Sed lectus vestibulum mattis.
+          </p>
+          <br></br>
+          <p className='data-text'>
+            Egestas erat imperdiet sed euismod nisi porta lorem mollis. Felis
+            donec et odio pellentesque diam volutpat commodo sed. Ante in nibh
+            mauris cursus mattis molestie a. Risus ultricies tristique nulla
+            aliquet enim tortor at auctor urna. Sed lectus vestibulum mattis.
+          </p>
+        </div>
       </div>
       <div className='home-info-tag-container'>
         <p className='info-tag'>[MTNT_dRamsey]..[ADMIN]</p>
