6e686b27f1a898e99665cf0511de9c3d69eeef8e
Casey The Great <casey@caseypitman.com>
2020-12-16 09:31:02 -0600

    Created characters.js for as character list.

--- src/characters.js
+++ src/characters.js
@@ -3,6 +3,10 @@
 const characters = {
   C: { id: "C", src: "../images/krakoan-characters/C.jpg", alt: "C character" },
   A: { id: "A", src: "../images/krakoan-characters/A.jpg", alt: "A character" },
+  S: { id: "S", src: "../images/krakoan-characters/S.jpg", alt: "S character" },
+  E: { id: "E", src: "../images/krakoan-characters/E.jpg", alt: "E character" },
+  S: { id: "Y", src: "../images/krakoan-characters/Y.jpg", alt: "Y character" },
+  
 };
 
 function imageLoader() {
