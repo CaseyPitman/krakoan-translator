92b689a4917de68b6dca5cf16ddbfee2dd007cb0
Casey The Great <casey@caseypitman.com>
2020-12-16 15:39:25 -0600

    Footer: layout and styles.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@
 
 import React from "react";
 
+// Styles
+import "../css/footer.css";
+
 const Footer = () => {
   return (
-    <div className = 'footer'>
+    <div className='footer'>
       <p className='copyright'>&copy; 2020 | Casey Pitman</p>
     </div>
   );
