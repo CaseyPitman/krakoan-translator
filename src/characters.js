b97b086821ddf6e6c91e388f2a283597f0439347
Casey The Great <casey@caseypitman.com>
2020-12-16 10:08:16 -0600

    Moved characters to public folder. Temporary fix. Will use webpack to import en masse.

--- src/characters.js
+++ src/characters.js
@@ -1,13 +1,12 @@
 //Object containing reference to each character
 
-const characters = {
-  C: { id: "C", src: "../images/krakoan-characters/C.jpg", alt: "C character" },
-  A: { id: "A", src: "../images/krakoan-characters/A.jpg", alt: "A character" },
-  S: { id: "S", src: "../images/krakoan-characters/S.jpg", alt: "S character" },
-  E: { id: "E", src: "../images/krakoan-characters/E.jpg", alt: "E character" },
-  S: { id: "Y", src: "../images/krakoan-characters/Y.jpg", alt: "Y character" },
-  
-};
+const characters = [
+  { id: "C", src: "../images/krakoan-characters/C.jpg", alt: "C character" },
+  { id: "A", src: "../images/krakoan-characters/A.jpg", alt: "A character" },
+  { id: "S", src: "../images/krakoan-characters/S.jpg", alt: "S character" },
+  { id: "E", src: "../images/krakoan-characters/E.jpg", alt: "E character" },
+  { id: "Y", src: "../images/krakoan-characters/Y.jpg", alt: "Y character" },
+];
 
 function imageLoader() {
   return characters;
--- src/components/EnglishTranslate.js
+++ src/components/EnglishTranslate.js
@@ -1,22 +1,19 @@
 //This Component translates from English to Krakoan
 
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+
+
+//Styles
+import '../css/english-translate.css'
+
 
 //Assets
-import imageLoader from '../characters.js'
-
+// import imageLoader from "../characters.js";
 
 //{ images.map(({id, src, title, description}) => <img key={id} src={src} title={title} alt={description} />)
 
 const EnglishTranslate = () => {
-const [characters, setCharacters] = useState([]);
-
-useEffect(()=>{
-   const importedCharacters = imageLoader();
-   setCharacters(importedCharacters);
-}, []);
-
-
+  //   const [characters, setCharacters] = useState([imageLoader()]);
 
   return (
     <div className='english-translate'>
@@ -25,10 +22,13 @@ useEffect(()=>{
           <textarea className='english-input'></textarea>
           <p className='info-tag'>[MTNT_dRamsey]..[ADMIN]</p>
         </div>
-        <div className = 'krakoan-output'>
-         {/* <img src = {} */}
+        <div className='krakoan-output'>
+          <img src={process.env.PUBLIC_URL + "/krakoan-characters/C.jpg"} className = 'k-output-character' />
+          <img src={process.env.PUBLIC_URL + "/krakoan-characters/A.jpg"} className = 'k-output-character'/>
+          <img src={process.env.PUBLIC_URL + "/krakoan-characters/S.jpg"} className = 'k-output-character'/>
+          <img src={process.env.PUBLIC_URL + "/krakoan-characters/E.jpg"} className = 'k-output-character'/>
+          <img src={process.env.PUBLIC_URL + "/krakoan-characters/Y.jpg"} className = 'k-output-character'/>
         </div>
-
       </div>
     </div>
   );
